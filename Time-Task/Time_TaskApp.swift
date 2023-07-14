85878a93a4ca0df42a0aeeb9010fcc9d128c2936
Omar Tharwat <77683787+omartharwat74@users.noreply.github.com>
2023-07-14 03:12:18 +0300

    Initial Commit

---
+++ Time-Task/Time_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_TaskApp.swift
+//  Time-Task
+//
+//  Created by Omar Tharwat on 14/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct Time_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
